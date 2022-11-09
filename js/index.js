d55268153cb080075a4924f77f1e2eb843f7d8a6
castell0n <castllon463@gmail.com>
2022-11-09 05:57:00 -0600

    actualización del proyecto completo

--- js/index.js
+++ js/index.js
@@ -1,39 +1,57 @@
-const newNota = document.getElementById("nueva_nota");
-const contenLis = document.getElementById("list");
-const eliminar = document.getElementsByClassName("close");
+const db = firebase.firestore();
 
-console.log(nota);
 
-var i;
-for (i = 0; i < eliminar.length; i++) {
-  eliminar[i].onclick = function() {
-    let div = this.parentElement;
-    div.remove(div);
+// <!-- Script -->
+const loader = document.querySelector('.spinner');
+const cuerpo = document.querySelector('body');
+window.onload = function(){
+  setTimeout(function(){
+    loader.style.display = 'none';
+    cuerpo.style.overflow = 'auto';
+  }, 4000); // Puedes cambiar el tiempo cada mil es 1 segundo.
+}
+
+const inputNewNota = document.getElementById("nueva_nota");
+const contenLis = document.getElementById("content_list");
+const eliminar = document.getElementsByClassName("btn-eliminar");
+document.getElementById("btn_agregar").addEventListener("click", btnAgregar);
+
+// Add a new document in collection "cities"
+function btnAgregar(){
+  if (inputNewNota.value == "") {
+    alert("campo vacio")
+  } else {
+    db.collection("MisTareas").doc().set({
+      tarea: inputNewNota.value,
+    })
+    .then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    });
+    inputNewNota.value= "";
   }
 }
 
+db.collection("MisTareas").onSnapshot((querySnapshot) => {
+  contenLis.innerHTML = ` `;
+  querySnapshot.forEach((doc) => {
+    contenLis.innerHTML +=
+    `<span data-id="${doc.id}" class="flex items-center justify-between mb-[0.6rem] w-[100%] h-[2.5rem] bg-[#ffffff] text-[#191e21] rounded-[0.4rem] overflow-hidden">
+      <h4 class="text-start text-[15px] py-[0.2rem] pb-[0.3rem] px-[1rem] min-w-[80%] whitespace-nowrap truncate overflow-hidden">${doc.data().tarea}</h4>
+      <button class="btn-eliminar w-[40%] h-[100%] bg-[#ffffff] text-[#191e21]">
+          <i class="bi bi-x-circle-fill flex items-center justify-center"></i>
+      </button>
+    </span>`
+    ;
 
-function agregarNota() {
-    if (newNota.value === "") {
-        alert("El campo se encuentra vaío");
-    } else {
-        const newli = `
-        <span id="itemlist" class="item_list">
-            <h4 id="nota" class="nota">${newNota.value}</h4>
-            <button id="close" class="close">
-                <i class="bi bi-x-circle-fill"></i>    
-            </button>
-        </span>`;
-
-        contenLis.innerHTML += newli;
-
-        newNota.value = "";
-    
-        for (i = 0; i < eliminar.length; i++) {
-            eliminar[i].onclick = function() {
-                var div = this.parentElement;
-                div.remove(div);
-            }
-        }
-    }
-}
+    let btndelete = contenLis.querySelectorAll(".btn-eliminar");
+    btndelete.forEach(btn => {
+      btn.addEventListener("click", e => {
+        let id = e.target.parentElement.parentElement.getAttribute("data-id");
+        db.collection("MisTareas").doc(id).delete();
+      })
+    })
+  });
+});
